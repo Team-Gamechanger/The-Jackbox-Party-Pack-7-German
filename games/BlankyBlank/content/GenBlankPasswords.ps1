d5c70e34fe28e9874be69990f3758907c21cd987
Eric <eric.boingo@gmail.com>
2023-05-05 18:25:15 +0200

    Update GenBlankPasswords.ps1

--- games/BlankyBlank/content/GenBlankPasswords.ps1
+++ games/BlankyBlank/content/GenBlankPasswords.ps1
@@ -1,6 +1,6 @@
-$FileInput = (Get-Content -Path .\BlankyBlankPasswords.jet) | ConvertFrom-Json
+$FileInput = (Get-Content -Path .\FULLBlankyBlankPasswords.jet) | ConvertFrom-Json
 $temp = $FileInput.content | Where-Object { $_.us -ne $true }
 
 $OutputContent = New-Object -TypeName PSObject
 $OutputContent | Add-Member -MemberType NoteProperty -Name content -Value $temp 
-$OutputContent | ConvertTo-Json -Depth 4 | Out-File -Path .\output.json
+$OutputContent | ConvertTo-Json -Depth 4 | Out-File -Path .\BlankyBlankPasswords.jet
